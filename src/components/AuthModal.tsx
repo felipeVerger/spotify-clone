57a25fceb829f05c85111c551e03187dae316a72
felipeverger <felipeverger123@gmail.com>
2023-06-06 16:33:00 -0300

    user authenitcation done

---
+++ src/components/AuthModal.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
+
+import Modal from "./Modal"
+import { useRouter } from 'next/navigation';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import useAuthModal from '@/hooks/useAuthModal';
+import { useEffect } from 'react';
+
+
+const AuthModal:React.FC = ({}) => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+  const { onClose, isOpen } = useAuthModal();
+
+  useEffect(() => {
+    if (session) {
+        router.refresh();
+        onClose();
+    }
+  }, [session, router, onClose])
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+        onClose();
+    }
+  }
+
+  return (
+    <Modal
+        title="Welcome back"
+        description="Login to yout account"
+        isOpen={isOpen}
+        onChange={onChange}
+    >
+        <Auth
+            supabaseClient={supabaseClient}
+            providers={["github"]}
+            magicLink
+            theme='dark'
+            appearance={{
+                theme: ThemeSupa,
+                variables: {
+                    default: {
+                        colors: {
+                            brand: '#404040',
+                            brandAccent: '#22c55e'
+                        }
+                    }
+                }
+            }}
+        />
+    </Modal>
+  )
+}
+
+export default AuthModal
